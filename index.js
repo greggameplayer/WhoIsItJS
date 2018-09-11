728b67936d02c4a13d3987635ce11a1e9c9b3131
greggameplayer <33609333+greggameplayer@users.noreply.github.com>
2018-09-12 00:29:43 +0200

    optimize and add some criteria

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-﻿var amixem, cyprien, norman, nozman, squeezie, answerwritter;
-var persos = [amixem, cyprien, norman, nozman, squeezie];
+﻿var answerwritter;
+var persos = [];
 var lunettes = [false, true, false, true, false];
 var barbe = [true, true, false, false, true];
+var haircolor = ["noir", "brun", "brun", "noir", "brun"];
+var eyescolor = ["marron", "gris", "gris", "marron", "gris"];
 var compteurclic = [0,0,0,0,0];
 var randomperso;
 var randomnumperso;
 var submitbutton;
 var textbox;
 function onLoad(){
-amixem = document.getElementById("amixem");
-cyprien = document.getElementById("cyprien");
-norman = document.getElementById("norman");
-nozman = document.getElementById("nozman");
-squeezie = document.getElementById("squeezie");
 submitbutton = document.getElementById("submit");
 textbox = document.getElementById("ask");
-persos = [amixem, cyprien, norman, nozman, squeezie];
+persos = [document.getElementById("amixem"), document.getElementById("cyprien"), document.getElementById("norman"), document.getElementById("nozman"), document.getElementById("squeezie")];
 randomnumperso = Math.floor(Math.random() * 5);
 randomperso = persos[randomnumperso];
 answerwritter = document.createElement("p");
@@ -32,32 +29,38 @@ persos[4].addEventListener("click", onClickSqueezie);
 window.addEventListener("load", onLoad);
 
 function onClickSubmitButton(){
-	if(textbox.value.toUpperCase() == "A T-IL DES LUNETTES ?" || textbox.value.toUpperCase() == "A T IL DES LUNETTES"){
-		if(lunettes[randomnumperso] == true){
-			answerwritter.innerHTML = "oui";
-		}else{
-			answerwritter.innerHTML = "non";
-		}
+	if(textbox.value.toUpperCase() === "A T-IL DES LUNETTES ?" || textbox.value.toUpperCase() === "A T IL DES LUNETTES" || textbox.value.toUpperCase().includes("LUNETTE") === true){
+        answerwritter.innerHTML = lunettes[randomnumperso] === true ? "oui" : "non";
 	}
-	
-	else if(textbox.value.toUpperCase() == "A T-IL UNE BARBE ?" || textbox.value.toUpperCase() == "A T IL UNE BARBE"){
-		if(barbe[randomnumperso] == true){
-			answerwritter.innerHTML = "oui";
-		}else{
-			answerwritter.innerHTML = "non";
-		}
+
+	else if(textbox.value.toUpperCase() === "A T-IL UNE BARBE ?" || textbox.value.toUpperCase() === "A T IL UNE BARBE" || textbox.value.toUpperCase().includes("BARBE") === true){
+        answerwritter.innerHTML = barbe[randomnumperso] === true ? "oui" : "non";
 	}
-	
+
+	else if(textbox.value.toUpperCase() === "A T-IL DES CHEVEUX NOIR ?" || textbox.value.toUpperCase() === "A T IL DES CHEVEUX NOIR" || textbox.value.toUpperCase().includes("CHEVEUX NOIR") === true){
+        answerwritter.innerHTML = haircolor[randomnumperso] === "noir" ? "oui" : "non";
+    }
+
+    else if(textbox.value.toUpperCase() === "A T-IL DES CHEVEUX BRUN ?" || textbox.value.toUpperCase() === "A T IL DES CHEVEUX BRUN" || textbox.value.toUpperCase().includes("CHEVEUX BRUN") === true){
+        answerwritter.innerHTML = haircolor[randomnumperso] === "brun" ? "oui" : "non";
+    }
+
+    else if(textbox.value.toUpperCase() === "A T-IL DES YEUX MARRON ?" || textbox.value.toUpperCase() === "A T IL DES YEUX MARRON" || textbox.value.toUpperCase().includes("YEUX MARRON") === true){
+        answerwritter.innerHTML = eyescolor[randomnumperso] === "marron" ? "oui" : "non";
+    }
+
+    else if(textbox.value.toUpperCase() === "A T-IL DES YEUX GRIS ?" || textbox.value.toUpperCase() === "A T IL DES YEUX GRIS" || textbox.value.toUpperCase().includes("YEUX GRIS") === true){
+        answerwritter.innerHTML = eyescolor[randomnumperso] === "gris" ? "oui" : "non";
+    }
+
 	else{
-		answerwritter.innerHTML = "Question invalide !";
+		answerwritter.innerHTML = 'Question invalide ! taper "aide" dans le champ de texte pour afficher les critères valide.';
 	}
-	
-	
-	
+
 }
 
 function onClickAmixem(){
-	if(compteurclic[0] == 0){
+	if(compteurclic[0] === 0){
 	var amixemcross = document.createElement("img");
 	amixemcross.src = "images/croix.png";
 	document.body.appendChild(amixemcross);
@@ -66,7 +69,7 @@ function onClickAmixem(){
 }
 
 function onClickCyprien(){
-	if(compteurclic[1] == 0){
+	if(compteurclic[1] === 0){
 	var cypriencross = document.createElement("img");
 	cypriencross.src = "images/croix.png";
 	document.body.appendChild(cypriencross);
@@ -75,7 +78,7 @@ function onClickCyprien(){
 }
 
 function onClickNorman(){
-	if(compteurclic[2] == 0){
+	if(compteurclic[2] === 0){
 	var normancross = document.createElement("img");
 	normancross.src = "images/croix.png";
 	document.body.appendChild(normancross);
@@ -84,7 +87,7 @@ function onClickNorman(){
 }
 
 function onClickNozman(){
-	if(compteurclic[3] == 0){
+	if(compteurclic[3] === 0){
 	var nozmancross = document.createElement("img");
 	nozmancross.src = "images/croix.png";
 	document.body.appendChild(nozmancross);
@@ -93,7 +96,7 @@ function onClickNozman(){
 }
 
 function onClickSqueezie(){
-	if(compteurclic[4] == 0){
+	if(compteurclic[4] === 0){
 	var squeeziecross = document.createElement("img");
 	squeeziecross.src = "images/croix.png";
 	document.body.appendChild(squeeziecross);
